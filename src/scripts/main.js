bc5c5954ca4203a4aecbce22c7747ab5ccf6baa2
Vladyslava Buzova <dev.vladyslava@gmail.com>
2022-08-08 01:07:14 +0800

    changed background

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,8 +6,8 @@ const btns = document.querySelectorAll('.question-btn');
 
 // click on the "plus" button => toggle(add/remove) class .show-text
 
-btns.forEach(function (btn) {
-  btn.addEventListener('click', function (e) {
+btns.forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
     const questions = e.currentTarget.parentElement.parentElement;
 
     questions.classList.toggle('show-text');
